bcf2b13cfc8b16a3d40b81e59d8e1c6c6e82a1f6
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-08-03 20:11:10 +0100

    done with notification services

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNotificationDto } from './dto/create-notification.dto';
-import { UpdateNotificationDto } from './dto/update-notification.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Notification } from './entities/notification.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class NotificationService {
-  create(createNotificationDto: CreateNotificationDto) {
-    return 'This action adds a new notification';
+  constructor(
+    @InjectModel(Notification.name)
+    private notificationModel: Model<Notification>,
+  ) {}
+
+  async createNotification(createNotificationDto: CreateNotificationDto) {
+    const notification = new this.notificationModel(createNotificationDto);
+    return await notification.save();
   }
 
-  findAll() {
-    return `This action returns all notification`;
+  async findAll() {
+    return this.notificationModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} notification`;
-  }
-
-  update(id: number, updateNotificationDto: UpdateNotificationDto) {
-    return `This action updates a #${id} notification`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} notification`;
+  async findOne(id: string) {
+    return this.notificationModel.findById(id).exec();
   }
 }
