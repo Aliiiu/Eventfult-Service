51185325727904ee6cd3b17dda773f59ca3dd64d
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-08-03 17:54:53 +0100

    done with event service and controller

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 
 @Controller('events')
+@UseGuards()
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
   @Post()
-  create(@Body() createEventDto: CreateEventDto) {
-    return this.eventsService.create(createEventDto);
+  create(@Body() createEventDto: CreateEventDto, @Request() req) {
+    return this.eventsService.create(createEventDto, req.user.id);
   }
 
   @Get()
@@ -19,16 +30,21 @@ export class EventsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.eventsService.findOne(+id);
+    return this.eventsService.findOne(id);
+  }
+
+  @Get(':creatorId')
+  findByCreator(@Param('creatorId') creatorId: string) {
+    return this.eventsService.findByCreator(creatorId);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
-    return this.eventsService.update(+id, updateEventDto);
+    return this.eventsService.update(id, updateEventDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.eventsService.remove(+id);
+    return this.eventsService.remove(id);
   }
 }
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,26 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Event } from './entities/event.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class EventsService {
-  create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+  constructor(
+    @InjectModel(Event.name) private readonly eventModel: Model<Event>,
+  ) {}
+
+  async create(createEventDto: CreateEventDto, creatorId: string) {
+    const createdEvent = new this.eventModel({
+      ...createEventDto,
+      creator: creatorId,
+    });
+    return createdEvent.save();
   }
 
-  findAll() {
-    return `This action returns all events`;
+  async findAll() {
+    return this.eventModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} event`;
+  async findOne(id: string) {
+    const event = this.eventModel.findById(id).exec();
+    if (!event) {
+      throw new Error(`Event with id ${id} not found`);
+    }
+    return event;
   }
 
-  update(id: number, updateEventDto: UpdateEventDto) {
-    return `This action updates a #${id} event`;
+  async findByCreator(creatorId: string) {
+    return this.eventModel.find({ creator: creatorId }).exec();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} event`;
+  async update(id: string, updateEventDto: UpdateEventDto) {
+    const updatedEvent = await this.eventModel
+      .findByIdAndUpdate(id, updateEventDto, { new: true })
+      .exec();
+
+    if (!updatedEvent) {
+      throw new NotFoundException(`Event with id ${id} not found`);
+    }
+    return updatedEvent;
+  }
+
+  async remove(id: string) {
+    const deletedEvent = await this.eventModel.findByIdAndDelete(id).exec();
+
+    if (!deletedEvent) {
+      throw new NotFoundException(`Event with id ${id} not found`);
+    }
+    return deletedEvent;
   }
 }
