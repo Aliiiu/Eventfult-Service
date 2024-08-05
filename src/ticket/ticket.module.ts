ac653fc989ea05347142c55f6f80ecdf33603452
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-08-06 00:50:25 +0100

    making qrcodemodule accessible to other modules

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -3,10 +3,12 @@ import { TicketService } from './ticket.service';
 import { TicketController } from './ticket.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Ticket, TicketSchema } from './entities/ticket.entity';
+import { QrCodeModule } from 'src/qr-code/qr-code.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Ticket.name, schema: TicketSchema }]),
+    QrCodeModule,
   ],
   controllers: [TicketController],
   providers: [TicketService],
