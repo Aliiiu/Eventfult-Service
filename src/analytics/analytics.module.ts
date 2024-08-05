8dab2f81e4356438a4373647b9186b7f60e63160
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-08-06 00:44:13 +0100

    updating analytics module

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsController } from './analytics.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Ticket, TicketSchema } from 'src/ticket/entities/ticket.entity';
+import { EventSchema } from 'src/events/entities/event.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Ticket.name, schema: TicketSchema },
+      { name: Event.name, schema: EventSchema },
+    ]),
+  ],
   controllers: [AnalyticsController],
   providers: [AnalyticsService],
 })
