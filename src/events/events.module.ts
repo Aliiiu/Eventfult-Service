e07a0e0ae1e2b12ea9cbb5c35023b9e4a6aa122d
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-08-06 00:36:40 +0100

    updating ticket module

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
+import { Event, EventSchema } from './entities/event.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }]),
+  ],
   controllers: [EventsController],
   providers: [EventsService],
 })
